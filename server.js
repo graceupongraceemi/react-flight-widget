a1854c23a527213df7e8d8151c54dbc5bd5cc4bb
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-29 13:17:11 -0800

    In server.js, ?page-size=6 is added to const options url.  And .env is created for Astra URL and TOKEN

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const app = express();
 
 app.get('/flights', (req, res) => {
   const options = {
-    url: 'https://3a617db3-2714-4aa4-9fab-a4cf30c5e979-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/flights/collections/departures',
+    url: `${process.env.URL}?page-size=6`,
     headers: {
       accept: 'application/json',
-      'X-Cassandra-Token':
-        'AstraCS:YwLMyGGfNagwAmIkWxKLkHuA:230b50347d7752f6029bbe141e866cc18fbcd6c38ce1f85d69725d60070b87d9'
+      'X-Cassandra-Token': process.env.TOKEN
     }
   };
 
