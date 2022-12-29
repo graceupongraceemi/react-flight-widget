cd3d14196797d95851064b35d3a3d59d53b700f3
Emiko Esther S <graceupongraceemi@gmail.com>
2022-12-29 12:56:44 -0800

    In server.js, express app.get const options added with headers from Astra DtaStax personal token obtained.  As well, axios is added to get response data from Astra Datastax

--- server.js
+++ server.js
@@ -6,4 +6,25 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/flights', (req, res) => {
+  const options = {
+    url: 'https://3a617db3-2714-4aa4-9fab-a4cf30c5e979-us-east1.apps.astra.datastax.com/api/rest/v2/namespaces/flights/collections/departures',
+    headers: {
+      accept: 'application/json',
+      'X-Cassandra-Token':
+        'AstraCS:YwLMyGGfNagwAmIkWxKLkHuA:230b50347d7752f6029bbe141e866cc18fbcd6c38ce1f85d69725d60070b87d9'
+    }
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 app.listen(PORT, () => console.log('running on port ' + PORT));
